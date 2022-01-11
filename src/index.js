50bd609781eb225a3e0fdcfee9e927d463102752
Vitalij <1281978@gmail.com>
2022-01-12 00:15:52 +0300

    feat: my solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let arr = n.toString().split("");
+    (arr[0] === '-') ? arr.shift(): null;
+    return +(arr.reverse().join(''));
+};
